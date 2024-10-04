be0b621a3c3eacbd91aed2fdd3b35f841918ddfe
Maxim <11919911+Makasimka@users.noreply.github.com>
2024-10-04 09:09:03 +0500

    homework-2 (fix 4)

--- scripts/app.js
+++ scripts/app.js
@@ -2,20 +2,35 @@ function htmlEntities(str) {
 	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
+function addComment(uName, uEmail, uComment) {
+	const elm = document.getElementById('comments_list');
+	if (!elm) {
+		return;
+	}
+
+	uName    = htmlEntities(uName || 'Anonymous').trim();
+	uEmail   = htmlEntities(uEmail|| 'address@mail.ru').trim();
+	uComment = htmlEntities(uComment).trim();
+	if (!uComment) {
+		return;
+	}
+
+	let tpl = '';
+	tpl += `<div class="user-info"><b>${uName}</b> <small>(<a href="mailto:${uEmail}">${uEmail}</a>)</small></div>`;
+	tpl += `<p>${uComment}</p>`;
+	tpl += `<div class="datetime"><time>${new Date().toLocaleString()}</time></div>`;
+	elm.innerHTML = `<article class="information-in block-margin">${tpl}</article>` + elm.innerHTML;
+}
+
 function bindForm(form) {
 	form.addEventListener('submit', (e) => {
-		const elm = document.getElementById('comments_list');
-		if (!elm) {
-			return;
-		}
-
 		e.preventDefault();
 		const data = new FormData(form);
-		let tpl    = '';
-		tpl += `<div class="user-info"><b>${htmlEntities(data.get('user_name') || data.get('user_surname'))}</b> <small>(${data.get('user_email') || 'address@mail.ru'})</small></div>`;
-		tpl += `<p>${htmlEntities(data.get('user_comment') || '...')}</p>`;
-		tpl += `<div class="datetime"><time>${new Date().toLocaleString()}</time></div>`;
-		elm.innerHTML = `<article class="information-in block-margin">${tpl}</article>` + elm.innerHTML;
+		addComment(
+			data.get('user_name') || data.get('user_surname'),
+			data.get('user_email'),
+			data.get('user_comment'),
+		);
 		form.reset();
 	});
 }
